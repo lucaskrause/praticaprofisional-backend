73e862045dd1684a0feffe6a4c539c815cb4ad66
lucaskrause <lucaskrause@cloudcrm.tech>
2021-02-24 23:30:52 -0300

    inicio DAO

---
+++ DAL/Daos/DAO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Daos
+{
+    public class DAO
+    {
+        public SqlConnection conexao { get; }
+
+        public DAO()
+        {
+            this.conexao = Conecta.CreateConnection();
+        }
+
+        public DAO(SqlConnection conexao)
+        {
+            this.conexao = conexao;
+
+        }
+
+
+        public virtual void Inserir(Object obj)
+        {
+
+        }
+
+        public virtual void Editar(Object obj)
+        {
+
+        }
+
+        public virtual void Excluir(Object obj)
+        {
+
+        }
+
+        public virtual object BuscarPorID(Object obj)
+        {
+            return null;
+        }
+
+        public virtual DataTable ListarTodos()
+        {
+            return null;
+        }
+
+        public virtual object Pesquisar(string obj)
+        {
+            return null;
+        }
+
+        protected SqlCommand CreateCommandTransaction(SqlTransaction transaction)
+        {
+            SqlCommand comando = this.conexao.CreateCommand();
+
+            comando.Transaction = transaction;
+
+            return comando;
+        }
+    }
+}
