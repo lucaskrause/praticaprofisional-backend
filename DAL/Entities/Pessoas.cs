cb2496167acc95820793188792bbda672bad3934
lucaskrause <lucasggkrause@gmail.com>
2021-03-11 19:10:22 -0300

    mais entitys

---
+++ DAL/Entities/Pessoas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    class AbstractEntity
+    {
+        [Required]
+        public string Pais { get; set; }
+
+        [Required]
+        public string Sigla { get; set; }
+
+        [Required]
+        public string DDI { get; set; }
+    }
+}
