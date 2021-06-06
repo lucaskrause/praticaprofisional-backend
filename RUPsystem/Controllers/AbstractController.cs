bdef8354c97bb5ce5f4a309eb3e2068eb66dc66e
lucaskrause <lucasggkrause@gmail.com>
2021-06-05 22:12:57 -0300

    AbstractController

--- RUPsystem/Controllers/AbstractController.cs
+++ RUPsystem/Controllers/AbstractController.cs
@@ -10,17 +10,5 @@ namespace RUPsystem.Controllers
     public abstract class AbstractController<T> : ControllerBase where T : AbstractEntity
     {
         public readonly IService<T> _service;
-
-        public abstract Task<IActionResult> ListarTodos();
-
-        public abstract Task<IActionResult> BuscarPorID(int codigo);
-
-        public abstract Task<IActionResult> Inserir(T entity);
-
-        public abstract Task<IActionResult> Editar(T entity);
-
-        public abstract Task<IActionResult> Excluir(int codigo);
-
-        public abstract Task<IActionResult> Pesquisar(string str);
     }
 }
